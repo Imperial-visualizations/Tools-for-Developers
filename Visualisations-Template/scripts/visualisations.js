d2342d322497f8433cb439025794e8c1ab0f1454
Robert King <robertkng104@gmail.com>
2019-11-27 10:37:32 +0000

    added basic template

--- Visualisations-Template/scripts/visualisations.js
+++
@@ -1,7 +0,0 @@
-/*
- * Create your visualisation here! 
- * This file is already linked in the HTML file so you don't need to do anything
- * 
- * If you haven't used Javascript before make sure to look at the various tutorials on the #learning_materials channel on the Slack!
- * 
- */
